12469c16f4e54c7cf1302c335a3cb5b82f2fd3b5
craig201042 <walila12@yahoo.com.tw>
2024-05-03 10:00:30 +0800

    home page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,24 @@
 document.querySelector("#year").innerHTML = new Date().getFullYear();
 document.querySelector("#lastModified").innerHTML = new Date();
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+    if (modeButton.textContent.includes("🕶️")) {
+        main.style.background = "#000";
+        main.style.color = "#fff";
+        modeButton.textContent = "🔆";
+    } else {
+        main.style.background = "#eee";
+        main.style.color = "#000";
+        modeButton.textContent = "🕶️";
+    }
+});

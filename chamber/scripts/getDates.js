b566fc04d4e79f681168c077294fbd64bbcb5bc2
craig201042 <walila12@yahoo.com.tw>
2024-05-03 17:35:39 +0800

    home page

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,2 +1,10 @@
 document.querySelector("#year").innerHTML = new Date().getFullYear();
 document.querySelector("#lastModified").innerHTML = new Date();
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});

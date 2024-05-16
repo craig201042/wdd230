3dea6f0a086d068ce18804f426ff939a02952b42
craig201042 <walila12@yahoo.com.tw>
2024-05-17 06:16:51 +0800

    form

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,6 +3,9 @@ document.querySelector("#lastModified").innerHTML = new Date();
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 const visitsDisplay = document.querySelector(".visits");
+const p1 = document.querySelector("#pw");
+const p2 = document.querySelector("#pwc");
+const msg = document.querySelector("#message");
 
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
@@ -32,3 +35,25 @@ if (numVisits !== 0) {
 }
 numVisits++;
 localStorage.setItem("numVisits-ls", numVisits);
+
+p2.addEventListener("focusout", control);
+
+function control() {
+    if (p1.value != p2.value) {
+        msg.textContent = "❗Password do not match!";
+        p2.value = "";
+        p2.focus();
+    } else {
+        msg.textContent = "Password match!🥳";
+    }
+}
+
+const rangevalue = document.querySelector("#rangevalue");
+const range = document.querySelector("#pr");
+
+range.addEventListener("change", displayRatingValue);
+range.addEventListener("input", displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
--- week03/scripts/getDates.js
+++ week03/scripts/getDates.js
@@ -1,2 +1,3 @@
 document.querySelector("#year").innerHTML = new Date().getFullYear();
 document.querySelector("#lastModified").innerHTML = new Date();
+
